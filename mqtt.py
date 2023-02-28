aec46fbd4e14f3f723bd0b43de60353c8d126308
Josia Orava <josia.orava@gmail.com>
2023-02-28 12:01:19 +0200

    initial

---
+++ mqtt.py
@@ -0,0 +1,121 @@
+import network
+import socket
+import ssd1306
+from time import sleep
+import machine
+import json
+from umqtt.simple import MQTTClient
+
+
+
+# Network
+ssid = "KME662"
+pwd = "SmartIot"
+
+
+# Display
+i2c = machine.I2C(1, sda=machine.Pin(14), scl=machine.Pin(15))
+display = ssd1306.SSD1306_I2C(128, 64, i2c)
+
+# Buttons
+SW0 = machine.Pin(9, machine.Pin.IN, machine.Pin.PULL_UP)
+
+# Leds
+D1 = machine.PWM(machine.Pin(22, machine.Pin.OUT))
+D2 = machine.PWM(machine.Pin(21, machine.Pin.OUT))
+D3 = machine.PWM(machine.Pin(20, machine.Pin.OUT))
+
+# JSON msg
+msg = '{"topic":"josiaPico","msg":"message from Pico"}'
+
+# Global
+ip = ''
+
+# Const
+PWM_VALUE = 65535
+
+
+# Connect to class wlan
+def connect():
+    wlan = network.WLAN(network.STA_IF)
+    wlan.active(True)
+    wlan.connect(ssid, pwd)
+    while wlan.isconnected() == False:
+        print("Waiting...")
+        sleep(1)
+    print("Connected")
+    global ip
+    ip = wlan.ifconfig()[0]
+    displayIP(ip)
+    
+    
+def displayIP(ipAdd):
+    display.text("IP:", 0, 1)
+    display.text(str(ipAdd), 0, 10)
+    display.show()
+    
+def displayLEDS():
+    display.fill(0)
+    displayIP(ip)
+    d1Val = D1.duty_u16() / PWM_VALUE * 100
+    d2Val = D2.duty_u16() / PWM_VALUE * 100
+    d3Val = D3.duty_u16() / PWM_VALUE * 100
+    display.text("D1:", 0, 25)
+    display.text("D2:", 0, 35)
+    display.text("D3:", 0, 45)
+    display.text(str(int(d1Val + 1)), 25, 25)
+    display.text(str(int(d2Val + 1)), 25, 35)
+    display.text(str(int(d3Val + 1)), 25, 45)
+    display.text("%", 50, 25)
+    display.text("%", 50, 35)
+    display.text("%", 50, 45)
+    display.show()
+    
+    
+def sub_cb(topic, msg):
+    print((topic, msg))
+    string = msg.decode('UTF-8')
+    led, brightness = string.split(";")
+    
+    brightness = float(brightness) / 100
+    print(brightness)
+    brightness = brightness * PWM_VALUE
+    print(brightness)
+    if led == "D1":
+        D1.duty_u16(int(brightness - 1))
+    elif led == "D2":
+        D2.duty_u16(int(brightness - 1))
+    elif led == "D3":
+        D3.duty_u16(int(brightness - 1))
+    displayLEDS()
+    
+
+
+    
+def main():
+    
+    displayLEDS()
+    connect()
+        
+    Client = MQTTClient("343gr3", "192.168.1.254")
+    Client.set_callback(sub_cb)
+    Client.connect()
+    Client.subscribe(b"josia/LED")
+    while True:
+        
+        if SW0.value() == 0:
+            tmp = json.loads(msg)
+            Client.publish(tmp["topic"], tmp["msg"])
+            print("succeed")
+            break
+        Client.check_msg()
+        sleep(1)
+        
+        
+                
+                
+    Client.disconnect()
+    
+if __name__ == "__main__":
+    main()
+    
---
+++ node server/index.js
@@ -0,0 +1,105 @@
+const express = require('express');
+const mqtt = require('mqtt');
+const path = require('path');
+const util = require('util');
+const fs = require('fs');
+const bodyParser = require('body-parser');
+const app = express();
+
+const readFile = util.promisify(fs.readFile);
+const writeFile = util.promisify(fs.writeFile);
+
+
+
+app.use(bodyParser.json());
+function read(filePath = './message.json') {
+    return readFile(path.resolve(__dirname, filePath)).then(data => JSON.parse(data));
+}
+function write(data, filePath = './message.json') {
+    return writeFile(path.resolve(__dirname, filePath), JSON.stringify(data));
+}
+
+// create an MQTT instance
+const client = mqtt.connect('mqtt://192.168.1.254');
+const title = 'josiaPico';
+
+// Check that you are connected to MQTT and subscribe to a topic (connect event)
+client.on('connect', () => {
+    client.subscribe(title);
+})
+
+// handle instance where MQTT will not connect (error event)
+client.on('error', (err) => {
+    console.log('Error: ', err);
+})
+// Handle when a subscribed message comes in (message event)
+client.on('message', async (topic, message) => {
+    try{
+        const messages = await read();
+        console.log(messages);
+        messages.push({"id": randomID(6), "msg": message.toString()});
+        write(messages);
+    } catch(e){
+        console.log('Error: ' + e);
+    }
+})
+// Route to serve the home page
+app.get('/',(req,res) => {
+    res.sendFile('index.html', {root: __dirname});
+})
+
+// route to serve the JSON array from the file message.json when requested from the home page
+app.get('/messages', async (res,req) =>{
+    let messages = await read();
+    req.send(messages);
+})
+// Route to serve the page to add a message
+app.get('/add', (req,res)=> {
+    res.sendFile('message.html', {root: __dirname});
+})
+//Route to show a selected message. Note, it will only show the message as text. No html needed
+app.get('/:id', async (req,res) => {
+    const messages = await read();
+    res.send(messages.filter(c => c.id === req.params.id));
+})
+
+// Route to CREATE a new message on the server and publish to mqtt broker
+app.post('/', async (req,res) => {
+    let title = req.body.topic;
+    let msg = req.body.msg;
+    try{
+        client.publish(title, msg);
+        res.sendStatus(200);
+    } catch(e){
+        console.log('Error: ' + e);
+        res.sendStatus(200);
+    }
+    
+})
+
+// Route to delete a message by id (Already done for you)
+
+app.delete('/:id', async (req, res) => {
+    try {
+        const messages = await read();
+        write(messages.filter(c => c.id !== req.params.id));
+        res.sendStatus(200);
+    } catch (e) {
+        res.sendStatus(200);
+    }
+});
+
+function randomID(length){
+    let result = '';
+    let chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let charLength = chars.length;
+
+
+    for ( let i = 0; i < length; i++ ) {
+        result += chars.charAt(Math.floor(Math.random() * charLength));
+    }
+    return result;
+}
+
+// listen to the port
+app.listen(3000);
